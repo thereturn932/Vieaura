727662f00259a44d0d049d9babf91f3680558a81
thereturn932 <mburakozenn@gmail.com>
2021-12-01 22:37:21 +0300

    first commit

---
+++ Contracts/Vieaura.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Vieaura {
+    constructor() {
+        console.log("Contract is started");
+    }
+
+    function deposit(uint256 underlyingAmount) external {
+        /**
+        Deposits "underlyingAmount" of DAI token and recieves LP
+         */
+        
+        
+    }
+
+    function withdraw(uint256 lpAmount) external {
+        /**
+        Withdraws "lpAmount" of DAI token and recieves LP
+
+         */
+    } 
+
+    function harvest() external {
+        /**
+        claims the accumulated CRV rewards from Curve and
+        converts them to DAI 
+         */
+    }
+
+    function exchangeRate() public view {
+        /**
+        returns the exchange rate between the underlying
+        token (DAI) and the LP token 
+         */
+    }
+}
